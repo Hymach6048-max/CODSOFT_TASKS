e867f90b9a54ca252df7d96d4a1b6472e0f558a1
Hymach6048-max <hymach604@gmail.com>
2026-08-10 18:03:53 +0530

    Task3/BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,37 @@
+package Task3;
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean withdraw(double amount) {
+
+        if (amount <= 0) {
+            return false;
+        }
+
+        if (amount > balance) {
+            return false;
+        }
+
+        balance -= amount;
+        return true;
+    }
+
+    public boolean deposit(double amount) {
+
+        if (amount <= 0) {
+            return false;
+        }
+
+        balance += amount;
+        return true;
+    }
+}
